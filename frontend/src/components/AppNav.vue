826ae7f881b2db9cd8ad1723d9e46ab5a858e49e
SilverDay <klingner@silverday.de>
2026-04-08 21:46:08 +0200

    fix: account page shows Worlds link when no active world

--- frontend/src/components/AppNav.vue
+++ frontend/src/components/AppNav.vue
@@ -32,6 +32,7 @@ async function signOut() {
     <div class="app-nav__links">
       <RouterLink v-if="wid && !isDashboard" :to="`/worlds/${wid}`" class="btn btn-ghost btn-sm">Dashboard</RouterLink>
       <RouterLink v-if="isAccount && worlds.currentWorldId" :to="`/worlds/${worlds.currentWorldId}`" class="btn btn-ghost btn-sm">Dashboard</RouterLink>
+      <RouterLink v-if="isAccount && !worlds.currentWorldId" to="/worlds" class="btn btn-ghost btn-sm">Worlds</RouterLink>
       <RouterLink v-if="!isAccount" to="/account/settings" class="btn btn-ghost btn-sm">Account</RouterLink>
       <button class="btn btn-ghost btn-sm" @click="signOut">Sign out</button>
       <button class="btn btn-ghost btn-sm app-nav__theme" @click="toggle" :aria-label="'Toggle theme'">{{ themeIcon }}</button>
