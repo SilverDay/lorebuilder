61a3c6f056f123fb9f4eb17ec4e1ca35ac315e16
SilverDay <klingner@silverday.de>
2026-04-08 22:11:47 +0200

    fix: declare retryAfter and field properties on AuthException

--- core/Auth.php
+++ core/Auth.php
@@ -28,6 +28,9 @@ declare(strict_types=1);
 
 class AuthException extends \RuntimeException
 {
+    public ?float $retryAfter = null;
+    public ?string $field = null;
+
     public function __construct(
         string $message,
         public readonly string $errorCode,
